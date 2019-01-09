74d925db509d41bdb35c5083913d7b7f490a4075
Алексей Бельченков <u608110@gmail.com>
2019-01-09 18:56:49 +0400

    Adding More Routes to the API

---
+++ api/routes/orders.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET request to /orders'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST request to /orders'
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'Order details',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted order!'
+    });
+
+});
+
+module.exports = router;
